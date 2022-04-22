28603c84f22834394990b8f5111f0c46515167dc
oscarguifarro <oscarguifarro@localhost>
2022-04-22 15:17:09 -0600

    clase 1

---
+++ src/laboratorio_progra1/Laboratorio_progra1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_progra1;
+
+/**
+ *
+ * @author oscarguifarro
+ */
+public class Laboratorio_progra1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+ System.out.println("Hola Mundo! ");
+ System.out.println((10+12.34)+'$');
+ System.out.print("ESTUD \nIANTES  " );
+}
+
+}
+
+        // TODO code application logic here
+  
+    
+
